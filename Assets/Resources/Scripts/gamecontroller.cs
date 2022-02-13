865122b834399b606eea809df52776f5eef8d92b
Gerard Said <Gerard.Said@mcast.edu.mt>
2022-02-13 12:05:23 +0100

    Implementation of finished application

---
+++ Assets/Resources/Scripts/gamecontroller.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gamecontroller : MonoBehaviour {
+
+	//squareprefab is the file, square is what is drawn on screen
+	GameObject squarePrefabwithNumber,square;
+
+	Text clockText;
+	int counter,numbersquares = 0;
+
+	int totalsquares = 25;
+
+	public List<Vector3> positions;
+
+	Vector3 randomPosition;
+
+	bool check,cancreate=false;
+
+	// Use this for initialization
+	void Start () {
+		
+		//get the square prefab
+		squarePrefabwithNumber = Resources.Load("Prefabs/Squarewithnumber") as GameObject;
+		//clock text
+		clockText = GameObject.Find("Clock").GetComponent<Text>();
+		
+		//add the squarecontroller script to the square generated at the beginning ONLY
+		square.AddComponent<squarecontroller>();
+		//start the timer
+		StartCoroutine(updateClock());
+
+		//create a new list of positions for the placement of the squares
+		positions = new List<Vector3>();
+		
+		//initialize squares
+		numbersquares =1;
+		StartCoroutine(createSquares(numbersquares));
+		
+		
+	}
+
+	IEnumerator createSquares(int numsquares)
+	{
+		//modify this code such that blocks CANNOT spawn on top of each other
+		for(int i=0;i<numsquares;i++)
+		{
+			Debug.Log(i);
+			do{
+				check = false;
+				randomPosition = new Vector3(Random.Range(-4.5f,4.5f),Random.Range(-4.5f,4.5f));
+				if (positions.Count==0){
+					check = true;
+				//	positions.Add(randomPosition);
+				}
+				else {
+					cancreate = true;
+					foreach (Vector3 position in positions)
+					{
+						if (Vector3.Distance(randomPosition,position)<1f)
+						{
+							Debug.Log("too close"+randomPosition);
+							cancreate = false;
+						//	yield return new WaitForSeconds(0.2f);	
+						}
+					}
+					if (cancreate)
+					{
+						check = true;
+
+					}
+				}
+					
+
+			}while(!check);
+			
+			createSquareWithNumber(randomPosition);
+			positions.Add(randomPosition);
+			yield return null;
+		}
+	}
+
+	IEnumerator updateClock()
+	{
+		float timer = 0f;
+		while(true)
+		{
+			timer++;
+			string minutes = Mathf.Floor(timer/60f).ToString("00");
+			string seconds = Mathf.Floor(timer%60f).ToString("00");
+			clockText.text = minutes +":"+seconds;
+			yield return new WaitForSeconds(1f);
+		}
+	}
+
+	
+	public GameObject createSquareWithNumber(Vector3 position)
+	{
+		counter++;
+		square = Instantiate(squarePrefabwithNumber,position,Quaternion.identity);
+		square.GetComponentInChildren<Text>().text = counter.ToString();
+		return square;
+	}
+
+	public void resetScene()
+	{
+			counter=0;
+			positions = new List<Vector3>();
+			foreach(GameObject line in GameObject.FindGameObjectsWithTag("line"))
+			{
+				Destroy(line);
+			}
+				
+			foreach(GameObject square in GameObject.FindGameObjectsWithTag("square"))
+			{
+				Destroy(square);
+			}
+			if (numbersquares<totalsquares){
+				numbersquares++;	
+			}
+			else{
+				numbersquares = 1;
+			}
+			StartCoroutine(createSquares(numbersquares));
+			
+
+
+	}
+
+	public void loadMenu()
+    {
+		SceneManager.LoadScene("menu");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//press the space bar to reset the scene
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			loadMenu();
+		}
+		
+
+
+	}
+}
---
+++ Assets/Resources/Scripts/squarecontroller.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class squarecontroller : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    GameObject line;
+    Material linematerial;
+    Vector3 mousePosition, squarePosition;
+    GameObject movingCircle,myCircle;
+    int clickcounter = 0;
+    List<Vector3> squarepositions;
+    int levelcounter = 1;
+
+
+    void Start()
+    {
+        //copy a reference to the list of positions that are stored in waypointgenerator
+        movingCircle = Resources.Load("Prefabs/MovingCircle") as GameObject;
+		myCircle = Instantiate(movingCircle, Vector3.zero, Quaternion.identity);
+		myCircle.GetComponent<SpriteRenderer>().enabled = false;
+        linematerial = Resources.Load("linematerial") as Material;
+		
+    }
+
+    public void drawLine(Vector3 startPoint, Vector3 endPoint)
+    {
+        line = new GameObject();
+        line.name = "Waypointline";
+        line.tag = "line";
+
+        line.AddComponent<LineRenderer>();
+        line.GetComponent<LineRenderer>().sharedMaterial = linematerial;
+        line.GetComponent<LineRenderer>().startWidth = 0.1f;
+        line.GetComponent<LineRenderer>().endWidth = 0.1f;
+
+        Vector3[] vertices = new Vector3[2];
+
+        vertices[0] = startPoint;
+        vertices[1] = endPoint;
+
+        line.GetComponent<LineRenderer>().SetPositions(vertices);
+    }
+
+    bool checkallsquaresred()
+    {
+        bool allsquaresred = true;
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("square"))
+        {
+            if (g.GetComponent<SpriteRenderer>().color != Color.red)
+            {
+                allsquaresred = false;
+            }
+        }
+        return allsquaresred;
+    }
+
+    IEnumerator moveCircle(GameObject circle, List<Vector3> positions)
+    {
+        if (circle != null)
+        {
+			circle.GetComponent<SpriteRenderer>().enabled = true;
+            for (int i = 1; i < positions.Count; i++)
+            {
+                float distance = 0;
+                while (distance < 1f)
+                {
+					Debug.Log(positions[i]);
+                    circle.transform.position = Vector3.Lerp(circle.transform.position, positions[i], distance);
+                    distance += 0.1f;
+                    yield return new WaitForSeconds(0.1f);
+                }
+                yield return null;
+            }
+           circle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x and y coordinates in pixels
+        mousePosition = new Vector3(Input.mousePosition.x,
+                    Input.mousePosition.y, 10f);
+        //screen points
+        //	Debug.Log(mousePosition.x + " "+mousePosition.y);
+
+        squarePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //	Debug.Log(squarePosition.x + " "+squarePosition.y);
+
+        transform.position = squarePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Camera.main.GetComponent<waypointgenerator>().copySquare(squarePosition);
+            //I want to detect whether or not I've hit a square
+            RaycastHit2D hit = new RaycastHit2D();
+            hit = Physics2D.Raycast(squarePosition, Vector3.forward);
+
+            //write what I hit in the console
+            if (hit.collider != null)
+            {
+                //first things first, can I click the box, is it the next box? 
+                squarepositions = Camera.main.GetComponent<gamecontroller>().positions;
+                Vector3 boxhitposition = hit.collider.transform.position;
+
+                if (Vector3.Distance(boxhitposition, squarepositions[clickcounter]) < 0.2f)
+                {
+                    hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+
+					//draw line to the previous box
+				
+
+                    clickcounter++;
+					Debug.Log(clickcounter);
+					if (clickcounter > 1){
+							drawLine(squarepositions[clickcounter-1],squarepositions[clickcounter-2]);
+					}
+                }
+
+
+            }
+            //**** can only be red if it is the next number ****//
+            //TASK
+            /*
+			Create an animation that generates a small circle which at the start of each level,
+			travels slowly from one box to the next until it reaches the last box, after which
+			it disappears.  You may use a coroutine to perform this task
+			 */
+            //if all the squares are red
+            if (checkallsquaresred())
+            {
+                clickcounter = 0;
+                //reset the scene and add a box
+                Camera.main.GetComponent<gamecontroller>().resetScene();	
+                //increase the level
+                levelcounter++;
+                //update the UI text with the level counter here
+                GameObject.Find("Level").GetComponent<Text>().text = "<b>" + levelcounter + "</b>";
+            }
+
+            //ADD a counter as to how many times resetscene was called, and I want you to show it in the TOP RIGHT of the screen
+            if (levelcounter > 1)
+            {
+                //I need to spawn the circle.
+                //place a circle in the first position
+                squarepositions = Camera.main.GetComponent<gamecontroller>().positions;
+				myCircle.transform.position = squarepositions[0];
+				StopAllCoroutines();
+                StartCoroutine(moveCircle(myCircle, squarepositions));
+            }
+        }
+    }
+}
